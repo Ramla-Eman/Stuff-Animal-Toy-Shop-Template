f2745a5d632582698f50f37868c3c4153a63d204
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-07 17:24:42 +0500

    shop page UI completed

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const navContent = document.getElementById('nav-content');
 navToggle.addEventListener('click', () => {
     navContent.classList.toggle('hidden');
 });
+
+// document.getElementById("shop-link").classList.add("active");
